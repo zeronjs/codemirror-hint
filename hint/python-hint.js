7005451065663481d3ccd18881b815323474e571
zeronjs <89117628+zeronjs@users.noreply.github.com>
2021-08-18 10:58:08 +0700

    Update python-hint.js

--- hint/python-hint.js
+++ hint/python-hint.js
@@ -43,7 +43,8 @@
   CodeMirror.pythonHint = pythonHint; // deprecated
   CodeMirror.registerHelper("hint", "python", pythonHint);
 
-  var pythonKeywords = "and as assert async await break class continue def del elif else except finally for from global if import in is lambda nonlocal|10 not or pass raise return try while with yield __import__ abs all any ascii bin bool breakpoint bytearray bytes callable chr classmethod compile complex delattr dict dir divmod enumerate eval exec filter float format frozenset getattr globals hasattr hash help hex id input int isinstance issubclass iter len list locals map max memoryview min next object oct open ord pow print property range repr reversed round set setattr slice sorted staticmethod str sum super tuple type vars zip";
+  var pythonKeywords = "and as assert async await break class continue def del elif else except finally for from global if import in is lambda nonlocal|10 not or pass raise return try while with yield __import__ abs all any ascii bin bool breakpoint bytearray bytes callable chr classmethod compile complex delattr dict dir divmod enumerate eval exec filter float format frozenset getattr globals hasattr hash help hex id input int isinstance issubclass iter len list locals map max memoryview min next object oct open ord pow print property range repr reversed round set setattr slice sorted staticmethod str sum super tuple type vars zip __debug__ Ellipsis False None NotImplemented True Any Callable Coroutine Dict List Literal Generic Optional Sequence Set Tuple Type Union "
+;
  var pythonKeywordsL = pythonKeywords.split(" ");
  var pythonKeywordsU =""; //pythonKeywords.toUpperCase().split(" ");
 //keyword co dau () dang sau 
